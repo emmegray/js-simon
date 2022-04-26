2e959dc5f19653b8bb253321d9cb1c762160acc1
emmegray <lunaienuso@libero.it>
2022-04-26 16:42:36 +0200

    Fixed script, index and added style

--- script.js
+++ script.js
@@ -5,3 +5,51 @@
 4. Dopo l'inserimento dei 5 numeri il software dice quanti e quali dei numeri da indovinare sono stati individuati
 */
 
+// Creazione numeri casuali in pagina
+const elementoNumeri = document.getElementById("numeriDaIndovinare");
+const elementoNumeriInseriti = document.getElementById("numeriInseriti");
+const elementoPunteggio = document.getElementById("punteggio");
+const numeriDaGenerare = 5;
+let arrayNumeri = [];
+const numeriUtente = [];
+let totaleNumeriIndovinati = 0;
+
+for (let index = 0; index < 5; index++) {
+    arrayNumeri.push(Math.floor((Math.random()* 4)+1))    
+}
+
+elementoNumeri.innerHTML = `Simon Says: ${arrayNumeri}`;
+
+setTimeout(() => {
+    //Timer 5 secondi
+    elementoNumeri.innerHTML = "";
+}, 5000);
+
+setTimeout(() => {
+    //Inserimento dei numeri dall'utente
+    for (let index = 0; index < 5; index++) {
+        const inputUtente = prompt("Inserire numero da 1 a 5:");
+        numeriUtente.push(Number(inputUtente));
+    }
+
+    //Confronto numeri
+    for (let index = 0; index < arrayNumeri.length; index++) {
+        const numero = arrayNumeri[index];
+        const tentativo = numeriUtente[index];
+
+        const spanNumero = document.createElement("span");
+        spanNumero.textContent = tentativo;
+
+        if (tentativo == numero) {
+            //Hai indovinato
+            totaleNumeriIndovinati++;
+            spanNumero.className = "green";
+        } else {
+            //Non hai indovinato
+        }
+        elementoNumeriInseriti.appendChild(spanNumero);
+    }
+    //Risultato
+    elementoPunteggio.innerHTML = `Numeri indovinati totali: ${totaleNumeriIndovinati}`;
+}, 6000);
+
