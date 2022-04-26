21c2349e87b5c41806abb47d8c9adb3083455451
emmegray <lunaienuso@libero.it>
2022-04-26 14:49:55 +0200

    Added index and script

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Simon Says
+1. Mettere in pagina 5 numeri casuali
+2. Far partire un timer di 5 secondi
+3. Dopo i 5 secondi di timer l'utente inserisce uno alla volta i numeri che ha visto precedentemente in pagina, usando il prompt()
+4. Dopo l'inserimento dei 5 numeri il software dice quanti e quali dei numeri da indovinare sono stati individuati
+*/
+
